bc1285a6b7914c5c13fc6ff93b1674449b1e6af7
mohamed-fadl <mhharktat@gmail.com>
2022-07-21 12:50:51 +0200

    wip

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 var fetch = require('node-fetch');
 const notifier = require('node-notifier');
 var cron = require('node-cron');
+var moment = require('moment');
+
+
 var config = require('./conf.json');
 
 const refresh_period = 10;
+const url = config.url;
 
 const getCaseDetails = async () => {
-    const url = config.url;
-    const cookie = config.cookie;
-    const xrfToken = config.token;
     var response = await fetch(url, {
-        headers: {
-            Cookie: cookie,
-            X_XSRF_TOKEN: xrfToken
-        }
+        headers: getHeaders()
     });
 
     var responseBody = null;
     try {
         responseBody = await response.json();
-        console.log("Fetched update successfully");
+        printMessage("Fetched update!")
         handleResponse(responseBody);
     } catch (error) {
-        console.log("failed to fetch status update!");
+        printMessage("failed to fetch status update!");
         console.log(error);
         handleError();
     }
@@ -40,8 +38,34 @@ const handleError = () => {
     notifier.notify("ERROR :(, check the logs!")
 }
 
+const getHeaders = () => {
+    const cookie = config.cookie;
+    const xrfToken = config.token;
+     return {
+        Cookie: cookie,
+        X_XSRF_TOKEN: xrfToken
+    }
+}
+
+const refreshSession = async () => {
+    var response = null;
+    try {
+        response = await fetch(url, {
+            headers: getHeaders()
+        })
+        printMessage("Session refreshed!")
+    } catch (error) {
+        console.log("couldn't refresh session ", err)
+    }
+    
+}
+
+const printMessage = (msg) => {
+    console.log(`${moment().format("DD-MM-YYYY hh:mm:ss")} -> ${msg}`);
+}
 getCaseDetails();
 cron.schedule(`*/${refresh_period} * * * *`, () => {
+    refreshSession();
     getCaseDetails();
 });
 
